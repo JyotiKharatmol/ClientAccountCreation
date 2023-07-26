ff276861d852a9c27e0eb6d0ea5a39835ee3e69c
JyotiKharatmol <jyotikharatmol3095@gmail.com>
2023-07-27 03:03:55 +0530

    fifteenth commit

--- src/main/java/Resource_Properties/Base_File.java
+++ src/main/java/Resource_Properties/Base_File.java
@@ -28,7 +28,7 @@ public class Base_File
    { 
 	  ChromeOptions options = new ChromeOptions();
 	  
-	  options.addArguments("--headless","--no-sandbox");
+	  options.addArguments("--headless","--no-sandbox", "--disable-dev-shm-usage");
 	  
 	  WebDriverManager.chromedriver().setup();
 	  
