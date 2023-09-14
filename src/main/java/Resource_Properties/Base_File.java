91ab52a529a505271ecdfecec7928bad8e2bf479
JyotiKharatmol <jyotikharatmol3095@gmail.com>
2023-09-14 23:49:53 +0530

    14th sep 23:50

--- src/main/java/Resource_Properties/Base_File.java
+++ src/main/java/Resource_Properties/Base_File.java
@@ -29,7 +29,6 @@ public class Base_File
    {    
 	  ChromeOptions options = new ChromeOptions();
 	  
-	  /* 
 	  options.addArguments("--no-sandbox"); 
 	  options.addArguments("--disable-dev-shm-usage");
 	  options.addArguments("--headless");
@@ -37,7 +36,6 @@ public class Base_File
 	  options.addArguments("--disable-infobars");
 	  options.addArguments("--disable-extensions");
 	  options.addArguments("--window-size=1400, 600");
-	  */
 	  
 	  WebDriverManager.chromedriver().setup(); 
 	  driver = new ChromeDriver(options);
